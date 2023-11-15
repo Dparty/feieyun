f7c885b6b9064cb8f4fdf12bab0b762d9c3ceacf
Dan Chen <dan.chen@boardware.com>
2023-11-15 16:00:30 +0800

    feat

--- text.go
+++ text.go
@@ -2,6 +2,9 @@ package feieyun
 
 type PrintAble interface {
 	String() string
+	Text() string
+	Length() int
+	Width() int
 }
 
 const BR = "<BR>"
@@ -10,12 +13,44 @@ type Text struct {
 	Content string
 }
 
+func (t *Text) Text() string {
+	return t.Content
+}
+
+func (t *Text) Width() int {
+	var length int
+	for _, c := range t.Content {
+		if IsChineseChar(string(c)) {
+			length += 2
+		} else {
+			length += 1
+		}
+	}
+	return length
+}
+
 func (t *Text) String() string {
 	return t.Content
 }
 
+func (t *Text) Length() int {
+	return len(t.Content)
+}
+
 type Center struct {
 	Content PrintAble
+}
+
+func (c *Center) Text() string {
+	return c.Content.Text()
+}
+
+func (t *Center) Width() int {
+	return t.Content.Width()
+}
+
+func (c *Center) Length() int {
+	return c.Content.Length()
 }
 
 func (c *Center) String() string {
@@ -26,6 +61,18 @@ type Bold struct {
 	Content PrintAble
 }
 
+func (b *Bold) Text() string {
+	return b.Content.Text()
+}
+
+func (t *Bold) Width() int {
+	return t.Content.Width() * 2
+}
+
+func (c *Bold) Length() int {
+	return c.Content.Length()
+}
+
 func (b *Bold) String() string {
 	return "<B>" + b.Content.String() + "</B>"
 }
@@ -34,14 +81,38 @@ type CenterBold struct {
 	Content PrintAble
 }
 
+func (c *CenterBold) Text() string {
+	return c.Content.Text()
+}
+
+func (c *CenterBold) Width() int {
+	return c.Content.Width() * 2
+}
+
+func (c *CenterBold) Length() int {
+	return c.Content.Length()
+}
+
 func (c CenterBold) String() string {
 	return "<CB>" + c.Content.String() + "</CB>"
 }
 
 type Cut struct{}
 
+func (Cut) Text() string {
+	return ""
+}
+
 func (Cut) String() string {
 	return "<CUT>"
+}
+
+func (Cut) Width() int {
+	return 0
+}
+
+func (Cut) Length() int {
+	return 0
 }
 
 type Qrcode struct {
@@ -52,13 +123,29 @@ func (q Qrcode) String() string {
 	return "<QR>" + q.Content + "</QR>"
 }
 
+func NewDiv(width int) *Div {
+	return &Div{width}
+}
+
 type Div struct {
-	Width int64
+	width int
+}
+
+func (d *Div) Text() string {
+	return ""
+}
+
+func (d *Div) Width() int {
+	return d.Length()
+}
+
+func (d *Div) Length() int {
+	return d.width
 }
 
 func (d *Div) String() string {
 	div := ""
-	for i := 0; i < int(d.Width); i++ {
+	for i := 0; i < int(d.Width()); i++ {
 		div += "-"
 	}
 	div += BR
@@ -69,6 +156,18 @@ type Line struct {
 	Content PrintAble
 }
 
+func (l Line) Text() string {
+	return l.Content.Text()
+}
+
+func (l Line) Width() int {
+	return l.Content.Width()
+}
+
+func (l Line) Length() int {
+	return l.Content.Length()
+}
+
 func (l Line) String() string {
 	return l.Content.String() + BR
 }
@@ -77,14 +176,14 @@ type PrintContent struct {
 	Lines []PrintAble
 }
 
-func (p *PrintContent) AddLine(contents ...PrintAble) {
+func (p *PrintContent) AddLines(contents ...PrintAble) {
 	for _, content := range contents {
 		p.Lines = append(p.Lines, Line{content})
 	}
 }
 
-func (p *PrintContent) AddDiv(width int64) {
-	p.AddLine(&Div{Width: width})
+func (p *PrintContent) AddDiv(width int) {
+	p.AddLines(NewDiv(width))
 }
 
 func (p *PrintContent) String() string {
@@ -93,4 +192,49 @@ func (p *PrintContent) String() string {
 		s += line.String()
 	}
 	return s
+}
+
+type Row struct {
+	Columns []Column
+}
+
+func (r *Row) Width() int {
+	width := 0
+	for _, column := range r.Columns {
+		width += column.Width()
+	}
+	return width
+}
+
+func (r *Row) Length() int {
+	return r.Width()
+}
+
+func (r *Row) AddColumns(columns ...Column) {
+	r.Columns = append(r.Columns, columns...)
+}
+
+func (r *Row) String() {
+
+}
+
+type Column struct {
+	W int
+	P PrintAble
+}
+
+func (c Column) Strings() []string {
+	return []string{}
+}
+
+func (c Column) String() string {
+	return ""
+}
+
+func (c Column) Width() int {
+	return c.W
+}
+
+func (c Column) Length() int {
+	return c.Width()
 }
--- utils.go
+++ utils.go
@@ -3,10 +3,21 @@ package feieyun
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"regexp"
+	"unicode"
 )
 
 func SHA1(str string) string {
 	s := sha1.Sum([]byte(str))
 	strsha1 := hex.EncodeToString(s[:])
 	return strsha1
+}
+
+func IsChineseChar(str string) bool {
+	for _, r := range str {
+		if unicode.Is(unicode.Scripts["Han"], r) || (regexp.MustCompile("[\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]").MatchString(string(r))) {
+			return true
+		}
+	}
+	return false
 }
